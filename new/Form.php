4f5a51d7cf7976df8fbf9100151f55bfae9ba22a
Asha-asr <69521577+Asha-asr@users.noreply.github.com>
2021-02-04 16:15:57 +0530

    Create Form.php

---
+++ new/Form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert.php" method="post">
+        <label> First Name </label><br>
+        <input type="text" name="fname"><br>
+        <label> Last Name </label><br>
+        <input type="text" name="lname"><br>
+        <label> Email Address </label><br>
+        <input type="email" name="Email"><br>
+        <input type="submit" value="Submit">
+
+    </form>
+</body>
+</html>
